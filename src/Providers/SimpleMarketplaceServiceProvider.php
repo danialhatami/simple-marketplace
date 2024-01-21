f49f03786a9972055e14c61d3dae2c7492532826
Danial <danialhatami94@gmail.com>
2024-01-21 22:54:41 +0330

    modify package service provider

---
+++ src/Providers/SimpleMarketplaceServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Danial\SimpleMarketplace\Providers;
+
+use Danial\SimpleMarketplace\Console\Commands\InstallCommand;
+use Danial\SimpleMarketplace\Exceptions\CustomExceptionHandler;
+use Danial\SimpleMarketplace\Http\Middlewares\MustBeLoggedIn;
+use Danial\SimpleMarketplace\Models\Order;
+use Danial\SimpleMarketplace\Models\Product;
+use Danial\SimpleMarketplace\Policies\OrderPolicy;
+use Danial\SimpleMarketplace\Policies\ProductPolicy;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Foundation\Console\AboutCommand;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\SanctumServiceProvider;
+
+class SimpleMarketplaceServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../Routes/api.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        Route::middlewareGroup('bindings', [
+            SubstituteBindings::class,
+        ]);
+
+        Gate::policy(Order::class, OrderPolicy::class);
+        Gate::policy(Product::class, ProductPolicy::class);
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                InstallCommand::class,
+            ]);
+        }
+
+        AboutCommand::add('Simple Marketplace', fn () => [
+            'Version' => '0.0.1',
+            'Published By' => 'Danial Hatami'
+        ]);
+    }
+
+    public function register(): void
+    {
+        $this->app->singleton(
+            ExceptionHandler::class,
+            CustomExceptionHandler::class
+        );
+        $this->app->register(SanctumServiceProvider::class);
+    }
+}
