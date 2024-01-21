4c3e8aa2ad9615fc459401c7ce2d5c77344e0c3c
Danial <danialhatami94@gmail.com>
2024-01-22 00:17:14 +0330

    add cache to get product search list

--- src/Services/ProductService.php
+++ src/Services/ProductService.php
@@ -7,6 +7,7 @@ use Danial\SimpleMarketplace\Exceptions\DatabaseException;
 use Danial\SimpleMarketplace\Models\Product;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 
 class ProductService
@@ -40,6 +41,7 @@ class ProductService
         } catch (\Throwable $e) {
             throw new DatabaseException('There was an error in creating the article.');
         }
+        Cache::forget('products_search_*');
     }
 
     public function getUserProducts(User $user): LengthAwarePaginator
@@ -49,17 +51,21 @@ class ProductService
 
     public function searchProducts(array $params): LengthAwarePaginator
     {
-        return Product::query()
-            ->when($params['query'] ?? null, function ($query, $text) {
-                return $query->whereRaw("MATCH(title) AGAINST(? IN NATURAL LANGUAGE MODE)", [$text]);
-            })
-            ->when($params['max_price'] ?? null, function ($query, $maxPrice) {
-                return $query->whereRaw("price + delivery_price <= ?", [$maxPrice]);
-            })
-            ->when($params['user_id'] ?? null, function ($query, $userId) {
-                return $query->where('user_id', $userId);
-            })
-            ->orderByRaw("price + delivery_price " . ($params['sort_by_price'] ?? 'asc'))
-            ->paginate(config('marketplace.products_per_page'));
+        $cacheKey = 'products_search_' . md5(serialize($params));
+
+        return Cache::remember($cacheKey, now()->addMinutes(60), function () use ($params) {
+            return Product::query()
+                ->when($params['query'] ?? null, function ($query, $text) {
+                    return $query->whereRaw("MATCH(title) AGAINST(? IN NATURAL LANGUAGE MODE)", [$text]);
+                })
+                ->when($params['max_price'] ?? null, function ($query, $maxPrice) {
+                    return $query->whereRaw("price + delivery_price <= ?", [$maxPrice]);
+                })
+                ->when($params['user_id'] ?? null, function ($query, $userId) {
+                    return $query->where('user_id', $userId);
+                })
+                ->orderByRaw("price + delivery_price " . ($params['sort_by_price'] ?? 'asc'))
+                ->paginate(config('marketplace.products_per_page'));
+        });
     }
 }
