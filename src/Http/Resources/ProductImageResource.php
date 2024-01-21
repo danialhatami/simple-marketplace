333d267104dcacab83540d38c984f7716d36a3b7
Danial <danialhatami94@gmail.com>
2024-01-21 22:54:52 +0330

    image resource

---
+++ src/Http/Resources/ProductImageResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Danial\SimpleMarketplace\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductImageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'image_url' => asset('storage/' . $this->image_path)
+        ];
+    }
+}
