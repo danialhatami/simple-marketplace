008eb04f7d48322f4d46cfe4604715df5ab16732
Danial <danialhatami94@gmail.com>
2024-01-22 23:13:31 +0330

    modify send order handler to log fake mail

--- src/Listeners/SendOrderNotification.php
+++ src/Listeners/SendOrderNotification.php
@@ -3,14 +3,22 @@
 namespace Danial\SimpleMarketplace\Listeners;
 
 use Danial\SimpleMarketplace\Events\OrderCreated;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Notification;
 use Danial\SimpleMarketplace\Notifications\OrderPlacedNotification;
 
-class SendOrderNotification
+class SendOrderNotification implements ShouldQueue
 {
+    public string $queue = 'default';
+
     public function handle(OrderCreated $event): void
     {
-        Notification::route('mail', 'admin@example.com')
-        ->notify(new OrderPlacedNotification($event->order));
+        if (config('marketplace.send_real_email_notification')) {
+            Notification::route('mail', config('marketplace.admin_email'))
+                ->notify(new OrderPlacedNotification($event->order));
+        } else {
+            \Log::info('Email Sent');
+        }
     }
 }
