5c27d74c0ebfebf23fd341618ea085574ee647f0
Danial <danialhatami94@gmail.com>
2024-01-22 23:12:19 +0330

    add admin email to config file

--- src/Config/marketplace.php
+++ src/Config/marketplace.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-    'products_per_page' => 10
+    'products_per_page' => 10,
+    'admin_email' => env('ADMIN_EMAIL', 'admin@localhost.com'),
+    'send_real_email_notification' => false,
 ];
